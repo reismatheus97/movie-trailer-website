9be3d343c304cedf38ad0835f803c18bbe4d743a
Matheus Reis <matheus.reis@efabrika.com.br>
2018-11-03 22:53:49 -0300

    Adjustments do met PEP8 style guide requirements.

--- MovieTrailerWebsite/main.py
+++ MovieTrailerWebsite/main.py
@@ -16,7 +16,9 @@ API_KEY = "394f3a8e67543449583d84bcef8634a0"
 starWarsMoviesArray = []
 
 # Fetch "TMDb" API with the API KEY with query = "Star Wars".
-response = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query=Star%20Wars")
+response = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" +
+                        API_KEY +
+                        "&query=Star%20Wars")
 
 # Parse the response to a JSON.
 responseJson = json.loads(response.content)
@@ -25,31 +27,37 @@ responseJson = json.loads(response.content)
 totalPages = responseJson['total_pages']
 
 for currentPage in range(1, totalPages):
-    # Only in the first page the request goes without "page" query string parameter.
+    # Only in the first page the request goes
+    # without "page" query string parameter.
     if currentPage != 1:
-        response = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query=Star%20Wars&page=" + str(currentPage))  # NOQA
+        response = requests\
+                    .get("https://api.themoviedb.org/3/search/movie?api_key=" +
+                         API_KEY +
+                         "&query=Star%20Wars&page=" +
+                         str(currentPage))
         responseJson = json.loads(response.content)
 
     # For each item in results, create a Movie instance.
     for item in responseJson['results']:
         movie_id = item['id']
-        movie_title = item['title'].encode("ascii", errors="ignore").decode()  # Removing non-ASCII chars.
-        movie_storyline = item['overview']
+        movie_title = item['title']\
+            .encode("ascii", errors="ignore")\
+            .decode()  # Removing non-ASCII chars.
         movie_popularity = item['popularity']
-        vote_average = item['vote_average']
-        poster_image_url = 'https://image.tmdb.org/t/p/w300' + str(item['poster_path'])
+        poster_image_url = "https://image.tmdb.org/t/p/w300" +\
+                           str(item['poster_path'])
         movieInstance = movie.Movie(movie_id,
                                     movie_title,
-                                    movie_storyline,
                                     movie_popularity,
-                                    vote_average,
-                                    poster_image_url,
-                                    "")
+                                    poster_image_url)
 
+        # Add the instance do the movie array.
         starWarsMoviesArray.append(movieInstance)
 
 # Sort the movie array by the key "movie_popularity"
-starWarsMoviesArray = sorted(starWarsMoviesArray, key=lambda k: k['movie_popularity'], reverse=True)
+starWarsMoviesArray = sorted(starWarsMoviesArray,
+                             key=lambda k: k['movie_popularity'],
+                             reverse=True)
 
 # Open the movie trailer website's page.
 fresh_tomatoes.open_movies_page(starWarsMoviesArray)
